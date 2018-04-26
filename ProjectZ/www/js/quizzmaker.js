89f5273d72b951cfd1917af7a4fd7ace80cf1fdb
EmileFontanet <emile@infomaniak.ch>
2018-04-26 22:46:26 +0200

    Ajout du front end de l'interface quizz dans newtest mais pas encore de back end

--- ProjectZ/www/js/quizzmaker.js
+++ ProjectZ/www/js/quizzmaker.js
@@ -2,7 +2,7 @@ function onDeviceReady() {
     console.log('Device ready');
 }
 document.addEventListener('deviceready', onDeviceReady, false);
-var json1  = JSON.parse(json);
+/*var json1  = JSON.parse(json);
 console.log(json1);
 function makeBody(question){
     var card = '<div class="card text-white bg-primary mb-3 w-100 h-25">  <div class="card-header" id="theme">Header</div>  <div class="card-body">    <p class="card-text" id = "enonce">Some quick example text to build on the card title and make up the bulk of the cards content.</p></div></div>';
@@ -17,4 +17,4 @@ function displayQuestionEnvironment(question){
     $("#enonce").text(question["enonce"]);
     return;
 }
-displayQuestionEnvironment(json1["question1"]);
+displayQuestionEnvironment(json1["question1"]);*/
