e3b9d9078e4b47e378fd7eee08e5cca68d21b077
EmileFontanet <emile@infomaniak.ch>
2018-05-06 22:32:29 +0200

    minor updates

--- ProjectZ/www/js/quizzmaker.js
+++ ProjectZ/www/js/quizzmaker.js
@@ -3,17 +3,69 @@ function onDeviceReady() {
 }
 document.addEventListener('deviceready', onDeviceReady, false);
 console.log(json1);
-/*function makeBody(question){
-    var card = '<div class="card text-white bg-primary mb-3 w-100 h-25">  <div class="card-header" id="theme">Header</div>  <div class="card-body">    <p class="card-text" id = "enonce">Some quick example text to build on the card title and make up the bulk of the cards content.</p></div></div>';
-    return card;
+var i = 1;
+var actualQuestion = "question" + String(i);
+function displayQuestion(question) {
+    $('#rep1').text(question['reponseA']);
+    $('#rep2').text(question['reponseB'])
+    $('#rep3').text(question['reponseC'])
+    $('#rep4').text(question['reponseD'])
+    $('#rep5').text(question['reponseE'])
+    $('#enonce').text(question['enonce'])
+    $('#theme').text(question['theme'])
+}
+function fadeAndChangeQuestion(question){
+    $("#rep1").fadeOut(
+    function(){
+        $('#rep1').text(question['reponseA']);
+    });
+    $("#rep2").fadeOut(
+    function(){
+        $('#rep2').text(question['reponseB']);
+    });
+    $("#rep3").fadeOut(
+    function(){
+        $('#rep3').text(question['reponseC']);
+    });
+    $("#rep4").fadeOut(
+    function(){
+        $('#rep4').text(question['reponseD']);
+    });
+    if(question['type'] == 'A'){
+        $("#rep5").fadeOut(
+        function(){
+            $('#rep5').text(question['reponseE']);
+        });
+        $("#rep5").fadeIn();
+    }
+    else{
+        $("#lastRepDiv").fadeOut();
+    }
+    
+    $("#enonce").fadeOut(
+    function(){
+        $('#enonce').text(question['enonce']);
+    });
+    $("#theme").fadeOut('slow', 
+    function(){
+        $('#them').text(question['theme']);
+    });
+    
+    $("#rep1").fadeIn();
+    $("#rep2").fadeIn();
+    $("#rep3").fadeIn();
+    $("#rep4").fadeIn();
+    
+    $("#enonce").fadeIn();
+    $("#theme").fadeIn();
 }
 
-function displayQuestionEnvironment(question){
-    $("#questionZone").empty();
-    var body = makeBody(question);
-    $("#questionZone").prepend(body);
-    $("#theme").text(question["theme"]);
-    $("#enonce").text(question["enonce"]);
-    return;
+function uncheckCheckboxes(){
+    $(".custom-control-input").prop('checked', false);
 }
-displayQuestionEnvironment(json1["question1"]);*/
+$('#button').click(function() {
+    i = i + 1;
+    actualQuestion = "question" + String(i);
+    uncheckCheckboxes();
+    fadeAndChangeQuestion(json1[actualQuestion]);
+});
