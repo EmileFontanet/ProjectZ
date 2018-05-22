f89c767458f4420fac09d521061d39f3b9da3c1a
EmileFontanet <emile@infomaniak.ch>
2018-05-22 07:54:04 +0200

    changement de notation (passage de notation avec des crochets a dot notation pour le fichier quizmaker.js ce qui permet de faire denouveau tourner l'application sur telephone. Ajout du bouton pour verifier les reponses mais qui ne fonctionne pas pour l'instant

--- ProjectZ/www/js/quizzmaker.js
+++ ProjectZ/www/js/quizzmaker.js
@@ -1,43 +1,52 @@
 function onDeviceReady() {
     console.log('Device ready');
+   
 }
+
 document.addEventListener('deviceready', onDeviceReady, false);
 var theme = "microbiologie";
 var quizThemeName = 'quizTest' + String(theme);
-var numberOfQuestions = Object.keys(json1).length
-//si c'est la première fois qu'on fait ce quiz, on crée la variable en mémoire locale 
-if(localStorage.getItem(quizThemeName) === null){ 
-    var i = 1;
-    var currentQuestion = "question" + String(i);
-    var testDate = new Date();
-    testDate = String(testDate);
-    var quizTestObj = {};
-    quizTestObj[testDate] = {};
-    quizTestObj['unfinishedTest'] = testDate;
-    quizTestObj[testDate]['i'] = i;
-    localStorage.setItem(quizThemeName, JSON.stringify(quizTestObj));
-}
+var numberOfQuestions = Object.keys(json1).length;
 
-//si la variable en mémoire locale existe déjà, on regarde si il existe un test en cours d'exécution
-else{
-    var quizTestObj = JSON.parse(localStorage.getItem(quizThemeName));
-    if(quizTestObj['unfinishedTest'] !== 'none'){
-        var testDate = quizTestObj['unfinishedTest'];
-        var i = quizTestObj[testDate]['i'];
-        $('#progressbar').width(String(100*(i-1)/(numberOfQuestions-1)) +"%");
-        var currentQuestion = "question" + String(i);
-    }
-    else{
+//si c'est la première fois qu'on fait ce quiz, on crée la variable en mémoire locale 
+try{
+    if(localStorage.getItem(quizThemeName) === null){ 
         var i = 1;
         var currentQuestion = "question" + String(i);
         var testDate = new Date();
         testDate = String(testDate);
-        quizTestObj['unfinishedTest'] = testDate;
+        var quizTestObj = {};
         quizTestObj[testDate] = {};
-        quizTestObj[testDate]['i'] = i;
+        quizTestObj.unfinishedTest = testDate;
+        quizTestObj[testDate].i = i;
         localStorage.setItem(quizThemeName, JSON.stringify(quizTestObj));
     }
+
+    //si la variable en mémoire locale existe déjà, on regarde si il existe un test en cours d'exécution
+    else{
+        var quizTestObj = JSON.parse(localStorage.getItem(quizThemeName));
+        if(quizTestObj.unfinishedTest !== 'none'){
+            var testDate = quizTestObj.unfinishedTest;
+            var i = quizTestObj[testDate].i;
+            $('#progressbar').width(String(100*(i-1)/(numberOfQuestions-1)) +"%");
+            var currentQuestion = "question" + String(i);
+        }
+        else{
+            var i = 1;
+            var currentQuestion = "question" + String(i);
+            var testDate = new Date();
+            testDate = String(testDate);
+            quizTestObj.unfinishedTest = testDate;
+            quizTestObj[testDate] = {};
+            quizTestObj[testDate].i = i;
+            localStorage.setItem(quizThemeName, JSON.stringify(quizTestObj));
+        }
+    }
 }
+catch(err){
+    alert(err.message);
+    }
+
 var score = 0 ;
 if(i == 1){
     $('#lastquestionbutton').hide();
@@ -45,32 +54,32 @@ if(i == 1){
 
 
 function displayQuestion(question) {
-    $('#rep1').text(question["reponseA"]);
-    $('#rep2').text(question['reponseB']);
-    $('#rep3').text(question['reponseC']);
-    $('#rep4').text(question['reponseD']);
-    if(question['type'] == 'A'){
+    $('#rep1').text(question.reponseA);
+    $('#rep2').text(question.reponseB);
+    $('#rep3').text(question.reponseC);
+    $('#rep4').text(question.reponseD);
+    if(question.type == 'A'){
         $('#lastRepDiv').show();
-        $('#rep5').text(question['reponseE']);
+        $('#rep5').text(question.reponseE);
     }
     else{
         $('#lastRepDiv').hide();
     }
     
-    $('#enonce').text(question['enonce']);
-    $('#theme').text(question['theme']);
-    $('#questionType').text("Type " + question['type']);
+    $('#enonce').text(question.enonce);
+    $('#theme').text(question.theme);
+    $('#questionType').text("Type " + question.type);
     questionPosition = {1 : 'A', 2 : 'B', 3 : 'C', 4 : 'D', 5 : 'E'};
     quizTestObj[testDate][currentQuestion] = {};
-    quizTestObj[testDate][currentQuestion]['questionPosition'] = questionPosition;
+    quizTestObj[testDate][currentQuestion].questionPosition = questionPosition;
     
     return questionPosition;
 }
 function fadeAndChangeQuestion(question){
-    if (question['type'] == 'A'){
+    if (question.type == 'A'){
         $("#rep5").fadeOut(
         function(){
-            $('#rep5').text(question['reponseE']);
+            $('#rep5').text(question.reponseE);
         });
         $('#lastRepDiv').fadeIn();
         $("#rep5").fadeIn();
@@ -81,11 +90,11 @@ function fadeAndChangeQuestion(question){
     
     $("#enonce").fadeOut(
     function(){
-        $('#enonce').text(question['enonce']);
+        $('#enonce').text(question.enonce);
     });
     $("#questionType").fadeOut(
     function(){
-        $('#questionType').text("Type " + question['type']);
+        $('#questionType').text("Type " + question.type);
     });
     /*$("#theme").fadeOut('slow', 
     function(){
@@ -93,19 +102,19 @@ function fadeAndChangeQuestion(question){
     });*/
     $("#rep1").fadeOut(
     function(){
-        $('#rep1').text(question['reponseA']);
+        $('#rep1').text(question.reponseA);
     });
     $("#rep2").fadeOut(
     function(){
-        $('#rep2').text(question['reponseB']);
+        $('#rep2').text(question.reponseB);
     });
     $("#rep3").fadeOut(
     function(){
-        $('#rep3').text(question['reponseC']);
+        $('#rep3').text(question.reponseC);
     });
     $("#rep4").fadeOut(
     function(){
-        $('#rep4').text(question['reponseD']);
+        $('#rep4').text(question.reponseD);
     });
     $("#rep1").fadeIn();
     $("#rep2").fadeIn();
@@ -114,25 +123,25 @@ function fadeAndChangeQuestion(question){
     $("#enonce").fadeIn();
     $("#questionType").fadeIn();
    // $("#theme").fadeIn();
-    questionPosition = {1 : 'A', 2 : 'B', 3 : 'C', 4 : 'D', 5 : 'E'}
+    questionPosition = {1 : 'A', 2 : 'B', 3 : 'C', 4 : 'D', 5 : 'E'};
     quizTestObj[testDate][currentQuestion] = {};
-    quizTestObj[testDate][currentQuestion]['questionPosition'] = questionPosition;
+    quizTestObj[testDate][currentQuestion].questionPosition = questionPosition;
     return questionPosition;
 }
 function checkAnswers(questionPosition, answers, question){
-    var score = 0
+    var score = 0;
     var userTrueAnswers = [];
     var correspondanceRep = {'A' : 'reponseA', 'B' : 'reponseB', 'C' : 'reponseC', 'D' : 'reponseD', 'E' : 'reponseE', };
-    var correspondanceCheckbox = {"checkbox1" :  1, "checkbox2" :  2, "checkbox3" : 3, "checkbox4" :  4, "checkbox5" :  5, }
+    var correspondanceCheckbox = {"checkbox1" :  1, "checkbox2" :  2, "checkbox3" : 3, "checkbox4" :  4, "checkbox5" :  5 };
     answers.forEach(function(entry){        
         userTrueAnswers.push(questionPosition[correspondanceCheckbox[entry]]);
     });
-    var rightAnswers = question['bonneReponse'].split(',');
-    quizTestObj[testDate][currentQuestion]['userAnswers'] = userTrueAnswers;
-    quizTestObj[testDate][currentQuestion]['rightAnswers'] = rightAnswers;
+    var rightAnswers = question.bonneReponse.split(',');
+    quizTestObj[testDate][currentQuestion].userAnswers = userTrueAnswers;
+    quizTestObj[testDate][currentQuestion].rightAnswers = rightAnswers;
     rightAnswers.forEach(function(entry){
         if(userTrueAnswers.includes(entry)){
-            score +=1
+            score +=1;
         }
     });
    // console.log(score);
@@ -149,7 +158,7 @@ function getUserAnswers(){
     var answers = [];
     Object.keys(checkedCheckboxes).forEach(function(entry){
         if(!isNaN(entry)){
-            answers.push(checkedCheckboxes[entry]['id']);
+            answers.push(checkedCheckboxes[entry].id);
         }
         
     });
@@ -158,20 +167,40 @@ function getUserAnswers(){
 function uncheckCheckboxes(){
     $(".custom-control-input").prop('checked', false);
 }
+function checkCheckboxes(checkboxesToCheck){
+    checkboxesToCheck.forEach(function(entry){
+        $('#checkbox' + String(entry)).prop('checked', true);
+    }
+    );
+}
 $('#lastQuestionButton').click(function() {
     var answers = getUserAnswers();
+    var checkboxesToCheck = [];
     if(checkAnswers(questionPosition, answers, json1[currentQuestion])){
         console.log('Bonne réponse');
-        score += 1
+        score += 1;
     }
     else{
         console.log('Mauvaise réponse');
     }
     i -=1;
-    quizTestObj[testDate]['i'] = i;
+    quizTestObj[testDate].i = i;
     $('#progressbar').width(String(100*(i-1)/(numberOfQuestions)) +"%");
     currentQuestion = "question" + String(i);
     uncheckCheckboxes();
+    try{
+        var oldUserAnswers = quizTestObj[testDate][currentQuestion].userAnswers;
+        var oldQuestionPosition = quizTestObj[testDate][currentQuestion].questionPosition;
+        oldUserAnswers.forEach(function(entry){
+            checkboxpos = Object.keys(oldQuestionPosition).find(key => oldQuestionPosition[key] === entry);
+            checkboxesToCheck.push(checkboxpos);
+        });
+        checkCheckboxes(checkboxesToCheck);
+    }
+    
+    catch(err) {
+        console.log("no history");
+        }
     questionPosition = fadeAndChangeQuestion(json1[currentQuestion]);
     localStorage.setItem(quizThemeName, JSON.stringify(quizTestObj));
     if(i == 1){
@@ -181,22 +210,23 @@ $('#lastQuestionButton').click(function() {
 });
 $('#nextQuestionButton').click(function() {
     var answers = getUserAnswers();
+    var checkboxesToCheck = [];
     if(checkAnswers(questionPosition, answers, json1[currentQuestion])){
         console.log('Bonne réponse');
-        score += 1
+        score += 1;
     }
     else{
         console.log('Mauvaise réponse');
     }
     i += 1;
-    quizTestObj[testDate]['i'] = i;
+    quizTestObj[testDate].i = i;
     $('#progressbar').width(String(100*(i-1)/(numberOfQuestions)) +"%");
     if(i == 2){
         $('#lastquestionbutton').fadeIn();
     }
     if(i == numberOfQuestions +1){
         score = Math.round(score/numberOfQuestions*100);
-        quizTestObj['unfinishedTest'] = 'none';
+        quizTestObj.unfinishedTest = 'none';
         localStorage.setItem(quizThemeName, JSON.stringify(quizTestObj));
         score = 60;
         if(score >= 50){   
@@ -248,9 +278,22 @@ $('#nextQuestionButton').click(function() {
         
         
     }
-    else{
+    else {
         currentQuestion = "question" + String(i);
         uncheckCheckboxes();
+        try {
+            var oldUserAnswers = quizTestObj[testDate][currentQuestion].userAnswers;
+            var oldQuestionPosition = quizTestObj[testDate][currentQuestion].questionPosition;
+            oldUserAnswers.forEach(function(entry){
+                checkboxpos = Object.keys(oldQuestionPosition).find(key => oldQuestionPosition[key] === entry);
+                checkboxesToCheck.push(checkboxpos);
+            });
+            checkCheckboxes(checkboxesToCheck);
+        }
+        catch(err){
+            console.log("no history");
+            }
+        
         questionPosition = fadeAndChangeQuestion(json1[currentQuestion]);
         localStorage.setItem(quizThemeName, JSON.stringify(quizTestObj));
     }
@@ -260,7 +303,7 @@ $('#nextQuestionButton').click(function() {
     
 });
 $('#questionType').click(function(){
-    if(json1[currentQuestion]['type'] === "A"){
+    if(json1[currentQuestion].type === "A"){
         swal({
             text : "Une question de type A contient 5 propositions et une seule bonne réponse.",
             title : "Type A",
@@ -276,4 +319,7 @@ $('#questionType').click(function(){
             button : false
         });
     }
+});
+$('#checkAnswerButtonDiv').click(function(){
+    alert('check answer');
 })
