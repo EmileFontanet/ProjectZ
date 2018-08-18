ad3cb4232b6360fe22606614eebf54110a11b0cb
EmileFontanet <emile.fontanet@infomaniak.ch>
2018-08-18 12:50:55 +0200

    amélioration du recap, ouverture d'une alerte si l'on clique sur une des question. L'alerte indique l'enonce ainsi que les reponses a la questions, la reponse donnee par l'utilisateur et la bonne reponse et la justification

--- ProjectZ/platforms/browser/www/js/recap.js
+++ ProjectZ/platforms/browser/www/js/recap.js
@@ -16,6 +16,11 @@ function checkQuestionResult(question){
 }
 document.addEventListener("deviceready", onDeviceReady, false);
 
+//on crée l'url du questionnaire qu'on va utiliser 
+var script = "questions/microbiologie.json";
+
+//on charge le questionnaire en question
+$("head").append('<script type="text/javascript" src="' + script + '"></script>');
 var quizDate = window.localStorage.getItem("recapDate");
 var quizThemeName = window.localStorage.getItem("recapThemeName");
 var quizTestObj = JSON.parse(localStorage.getItem(quizThemeName));
@@ -31,32 +36,108 @@ for(i = 1; i < Object.keys(quizTestObj[quizDate]).length; i++){
     $("#questionBoxes").append("<div class = 'questionBox" + String(checkQuestionResult(quizTestObj[quizDate]["question"+String(i)])) + "' id = 'question" + String(i) + "'>Question " + String(i) + "</div>");
 }
 $(".questionBoxtrue").click(function(event){
+    var currentQuestion = json1[event.target.id];
+    var quizThemeName = window.localStorage.getItem("recapThemeName");
+    var quizTestObj = JSON.parse(localStorage.getItem(quizThemeName));
+    var quizDate = window.localStorage.getItem("recapDate");
+    var htmlRecap = `<tr> 
+                            <th colspan = "2">` + currentQuestion.enonce + `</th>
+                            </tr>
+                          <tr>
+                            <td>Réponse A</td>
+                            <td>` + currentQuestion.reponseA + `</td>
+                            
+                          </tr>
+
+                        <tr>
+                            <td>Réponse B</td>
+                            <td>` + currentQuestion.reponseB + `</td>
+                            
+                          </tr>
+                        <tr>
+                            <td>Réponse C</td>
+                            <td>` + currentQuestion.reponseC + `</td>
+                            
+                          </tr>
+                        <tr>
+                            <td>Réponse D</td>
+                            <td>` + currentQuestion.reponseD + `</td>
+                            
+                          </tr>
+
+                          <tr>
+                            <td>Votre réponse</td>
+                            <td>` + quizTestObj[quizDate][event.target.id].rightAnswers + `</td>
+                            
+                          </tr>
+                          <tr>
+                            <td>Bonne réponse</td>
+                            <td>` + currentQuestion.bonneReponse + `</td>
+                          </tr>
+                        <tr>
+                            <td>Justification</td>
+                            <td>` + currentQuestion.justification + `</td>
+                          </tr>`;
    swal({
        content:{
            element: "table",
            attributes: {
-               innerHTML: `<tr> 
-                            <th colspan = "2">Age</th>
-                            </tr>
-                          <tr>
-                            <td>Jill</td>
-                            <td>Smith</td>
-                            
-                          </tr>
-                          <tr>
-                            <td>Eve</td>
-                            <td>Jackson</td>
-                            
-                          </tr>
-                          <tr>
-                            <td>John</td>
-                            <td>Doe</td>
-                          </tr>`
+               innerHTML: htmlRecap
            }
        }
        
    })
 })
 $(".questionBoxfalse").click(function(event){
-   
+    var currentQuestion = json1[event.target.id];
+    var quizThemeName = window.localStorage.getItem("recapThemeName");
+    var quizTestObj = JSON.parse(localStorage.getItem(quizThemeName));
+    var quizDate = window.localStorage.getItem("recapDate");
+    var htmlRecap = `<tr> 
+                            <th colspan = "2">` + currentQuestion.enonce + `</th>
+                            </tr>
+                          <tr>
+                            <td>Réponse A</td>
+                            <td>` + currentQuestion.reponseA + `</td>
+                            
+                          </tr>
+
+                        <tr>
+                            <td>Réponse B</td>
+                            <td>` + currentQuestion.reponseB + `</td>
+                            
+                          </tr>
+                        <tr>
+                            <td>Réponse C</td>
+                            <td>` + currentQuestion.reponseC + `</td>
+                            
+                          </tr>
+                        <tr>
+                            <td>Réponse D</td>
+                            <td>` + currentQuestion.reponseD + `</td>
+                            
+                          </tr>
+
+                          <tr>
+                            <td>Votre réponse</td>
+                            <td>` + quizTestObj[quizDate][event.target.id].userAnswers + `</td>
+                            
+                          </tr>
+                          <tr>
+                            <td>Bonne réponse</td>
+                            <td>` + currentQuestion.bonneReponse + `</td>
+                          </tr>
+                        <tr>
+                            <td>Justification</td>
+                            <td>` + currentQuestion.justification + `</td>
+                          </tr>`;
+   swal({
+       content:{
+           element: "table",
+           attributes: {
+               innerHTML: htmlRecap
+           }
+       }
+       
+   })
 })
